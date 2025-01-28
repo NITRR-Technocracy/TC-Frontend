b18c726d77bcc9d2a8370b7c9575def92e078f0e
Aditya Kumar Sah <141633697+Aditya-kumar-sah@users.noreply.github.com>
2025-01-28 22:08:17 +0530

    Update WorkshopsForm.js

--- src/forms/WorkshopsForm.js
+++ src/forms/WorkshopsForm.js
@@ -71,7 +71,7 @@ const WorkShopsForm = () => {
 
     if (condition) {
       try {
-        const res = await axios.post(`/server/register?event=Workshops`, form, {
+        const res = await axios.post(`${backend}/register?event=Workshops`, form, {
           headers: {
             "Content-Type": "application/json",
           },
